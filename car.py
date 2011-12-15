ab6cc30718dd9505f12284e87d284ad75afd363c
Juggeli <juggeli@gmail.com>
2011-12-16 00:28:53 +0200

    asdf

--- car.py
+++ car.py
@@ -1,7 +1,7 @@
 import pygame
 from utils import *
 
-class SnowMan(pygame.sprite.Sprite):
+class Car(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = load_image('snowman.png', -1)
@@ -9,7 +9,7 @@ class SnowMan(pygame.sprite.Sprite):
         #self.area = screen.get_rect()
         self.area = pygame.Rect(400, 150, 400, 100)
         self.rect.topleft = 400, 150
-        self.speed = 1
+        self.move = 1
         self.dizzy = 0
         self.stopped = 0
         self.count = 0
@@ -28,11 +28,11 @@ class SnowMan(pygame.sprite.Sprite):
             self._spin()
             
     def _walk(self):
-        newpos = self.rect.move((self.speed, 0))
+        newpos = self.rect.move((self.move, 0))
         if not self.area.contains(newpos):
             if self.rect.left < self.area.left or self.rect.right > self.area.right:
-                self.speed = -self.speed
-                newpos = self.rect.move((self.speed, 0))
+                self.move = -self.move
+                newpos = self.rect.move((self.move, 0))
                 self.image = pygame.transform.flip(self.image, 1, 0)
         self.rect = newpos
 
@@ -51,4 +51,3 @@ class SnowMan(pygame.sprite.Sprite):
         if not self.dizzy:
             self.dizzy = 1
             self.original = self.image
-    
