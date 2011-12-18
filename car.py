050af1bb3d2b8c843aaeebfe73cb79b40cfa02f9
junnu <jkylmanen@gmail.com>
2011-12-18 18:04:42 +0200

    autoja muokattu

--- car.py
+++ car.py
@@ -4,13 +4,24 @@ from utils import *
 class Car(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_image('fedex_car.png', -1)
+        rnd = random.randint(0,3)
+        if rnd == 0: 
+            self.image, self.rect = load_image('fedex_car.png', -1)
+        elif rnd == 1:
+            self.image, self.rect = load_image('car.png', -1)
+        elif rnd == 2: 
+            self.image, self.rect = load_image('car_trailer.png', -1)
+        elif rnd == 3: 
+            self.image, self.rect = load_image('car_trailer_green.png', -1)
+        
+        
+        
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
         self.image = pygame.transform.flip(self.image, 1, 0)
         #self.area = pygame.Rect(0, 150, 400, 100)
         self.rect.topleft = -self.rect.width, 425
-        self.speed = 5
+        self.speed = 9
         self.driving = 1
         
     def update(self):
